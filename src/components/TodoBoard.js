a1342453e7751165b50a1003350c3c3d3adce714
Seokwon Choi <cswcoco03112@gmail.com>
2024-05-27 03:09:16 +0900

    first commit

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -1,11 +1,13 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 
-const TodoBoard = () => {
+const TodoBoard = ({todoList , deleteTask , toggleComplete}) => {
   return (
     <div>
       <h2>Todo List</h2>
+      {todoList.length>0?todoList.map((item,index)=><TodoItem item={item} deleteTask={deleteTask} key={index} toggleComplete={toggleComplete}/>): <h2>There is no Item to show</h2>}
       {/* <TodoItem/> will be here once we get the todoList */}
-      <h2>There is no Item to show</h2>
+     
     </div>
   );
 };
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,33 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import api from "../utils/api";
 
-const TodoItem = () => {
+const TodoItem = ({ item, deleteTask, toggleComplete }) => {
+  // const deleteTask = async () => {
+  //   const response = api.delete(`/tasks/${todoList[key]._id}`);
+  //   console.log("r", response);
+  //   setTodoList((await response).data.data);
+  // };
   return (
     <Row>
       <Col xs={12}>
-        <div className={`todo-item`}>
-          <div className="todo-content">밥먹기</div>
+        <div className={`todo-item ${item.isComplete ? "item-complete" : ""}`}>
+          <div className="todo-content">{item.task}</div>
 
           <div>
-            <button className="button-delete">삭제</button>
-            <button className="button-delete">끝남</button>
+            <button
+              className="button-delete"
+              onClick={() => deleteTask(item._id)}
+            >
+              삭제
+            </button>
+            <button
+              className="button-delete"
+              onClick={() => toggleComplete(item._id)}
+            >
+              {" "}
+              {item.isComplete ? `안끝남` : `끝남`}
+            </button>
           </div>
         </div>
       </Col>
